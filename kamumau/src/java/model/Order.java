835b118c645a358841a77d3dba5cd243af692371
ydhnwb <yudhanewbie@gmail.com>
2018-10-20 21:55:33 +0700

    progress order and transaction

--- kamumau/src/java/model/Order.java
+++ kamumau/src/java/model/Order.java
@@ -1,16 +1,3 @@
-/*
- * @author x201
- *  CREATE TABLE orders (
-    id int NOT NULL AUTO_INCREMENT,
-    no int NOT NULL,
-    user_id int NOT NULL,
-    created_at DATE NOT NULL,
-    updated_at DATE NOT NULL,
-    status VARCHAR(10) NOT NULL,
-    PRIMARY KEY (id)
-);
-);
- */
 package model;
 
 import java.sql.ResultSet;
@@ -19,26 +6,59 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 public class Order extends MyConnection{
     private final String tableName= "orders";
-    private final String tableTransaction= "transactions";
+    private final String tableTransaction= "transaction";
     public int no;
     public int user_id;
     public Date created_at;
     public Date updated_at;
     private String status="new";
+    public int buyer_id;
+    public String buyer_name;
+    public String user_name;
+    
+    public Order(int id,int no, int user_id, int buyer_id, Date created_at, Date updated_at) {
+        this.no = no;
+        this.id = id;
+        this.buyer_id = buyer_id;
+        this.user_id = user_id;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
 
-    /* this constructor defined 
-       this class to have same behaviour with it parents
-    etc
-    */
     public Order() {
         super();
     }
+
+    public int getBuyer_id() {
+        return buyer_id;
+    }
+
+    public void setBuyer_id(int buyer_id) {
+        this.buyer_id = buyer_id;
+    }
+
+    public String getBuyer_name() {
+        return buyer_name;
+    }
+
+    public void setBuyer_name(String buyer_name) {
+        this.buyer_name = buyer_name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
     
-    /*
-    this method returns property id set by setId
-    */
+    
+    
     public int getId() {
         return id;
     }
@@ -47,38 +67,60 @@ public class Order extends MyConnection{
         this.id = id;
     }
 
+    
+    
     public int getNo() {
         return no;
     }
 
-    public void setUser(int user_id) {
-        this.user_id= user_id;
-    }
-
-    public void setStatus(String status) {
-        this.status= status;
-    }
-
-    private String generateDate(){
-        Date date= new Date();
-        return String.format("%1$tY-%1$tm-%1$td", date);
-    }
-
-    private int generateNo(){
-        return (int) System.currentTimeMillis();
-    }
     public void setNo(int no) {
         this.no = no;
     }
-    public void setCreatedAt(Date date){
-        this.created_at= date;
-    }
-    public void setUpdatedAT(Date date){
-        this.updated_at= date;
+
+    public int getUser_id() {
+        return user_id;
     }
 
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public String generateDate(){
+        Date date= new Date();
+        return String.format("%1$tY-%1$tm-%1$td", date);
+    }
+    
+    public int generateNo(){
+        return (int) System.currentTimeMillis();
+    }
+    
+    
     public ArrayList<Order> all(int user_id){
-        String query = "SELECT * FROM " + tableName  + "WHERE user_id = " + user_id;
+        String query = "SELECT * FROM " + tableName  + " WHERE user_id = " + user_id + " ORDER BY id DESC";
         ArrayList<Order> orders = new ArrayList<>();
         try {
             Statement stmt = this.conn().createStatement();
@@ -86,9 +128,44 @@ public class Order extends MyConnection{
             while (res.next()) {
                 Order order = new Order();
                 order.setNo(res.getInt("no"));
-                order.setCreatedAt(res.getDate("created_at"));
-                order.setUpdatedAT(res.getDate("updated_at"));
+                order.setCreated_at(res.getDate("created_at"));
+                order.setUpdated_at(res.getDate("updated_at"));
                 order.setId(res.getInt("id"));
+                order.setUser_id(res.getInt("user_id"));
+                order.setStatus(res.getString("status"));
+                orders.add(order);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return orders;
+    }
+    
+    
+    
+    
+    public ArrayList<Order> allMyOrders(int user_id){
+        //String query = "SELECT id, no, user_id, (SELECT u.fullname from orders o INNER JOIN users u ON o.buyer_id = u.id "
+        //        + ") as buyer_id FROM " + tableName  + " WHERE user_id = " + user_id + " ORDER BY id DESC";
+        
+        String query = "SELECT o.id,o.no,o.user_id,o.buyer_id,u.full_name as seller_name, o.created_at, o.updated_at, o.status FROM orders o \n" +
+        "INNER JOIN users u ON o.user_id = u.id WHERE o.buyer_id = "+user_id+";";
+        
+        
+        ArrayList<Order> orders = new ArrayList<>();
+        try {
+            Statement stmt = this.conn().createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            while (res.next()) {
+                Order order = new Order();
+                order.setNo(res.getInt("no"));
+                order.setCreated_at(res.getDate("created_at"));
+                order.setUpdated_at(res.getDate("updated_at"));
+                order.setId(res.getInt("id"));
+                order.setUser_id(res.getInt("user_id"));
+                order.setBuyer_id(res.getInt("buyer_id"));
+                order.setUser_name(res.getString("seller_name"));
+                order.setStatus(res.getString("status"));
                 orders.add(order);
             }
         } catch (SQLException e) {
@@ -97,7 +174,38 @@ public class Order extends MyConnection{
         return orders;
     }
 
-    public ArrayList<Transaction> getTransactions(int order_id) {
+    public ArrayList<Order> allIncoming(int user_id){
+        //String query = "SELECT id, no, user_id, (SELECT u.fullname from orders o INNER JOIN users u ON o.buyer_id = u.id "
+        //        + ") as buyer_id FROM " + tableName  + " WHERE user_id = " + user_id + " ORDER BY id DESC";
+        
+        String query = "SELECT o.id,o.no,o.user_id,o.buyer_id,u.full_name as buyer_name, o.created_at, o.updated_at, o.status FROM orders o \n" +
+        "INNER JOIN users u ON o.buyer_id = u.id WHERE o.user_id = "+user_id+";";
+        
+        
+        ArrayList<Order> orders = new ArrayList<>();
+        try {
+            Statement stmt = this.conn().createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            while (res.next()) {
+                Order order = new Order();
+                order.setNo(res.getInt("no"));
+                order.setCreated_at(res.getDate("created_at"));
+                order.setUpdated_at(res.getDate("updated_at"));
+                order.setId(res.getInt("id"));
+                order.setUser_id(res.getInt("user_id"));
+                order.setBuyer_id(res.getInt("buyer_id"));
+                order.setBuyer_name(res.getString("buyer_name"));
+                order.setStatus(res.getString("status"));
+                orders.add(order);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return orders;
+    }
+    
+    
+    /*public ArrayList<Transaction> getTransactions(int order_id) {
         String query = "SELECT * FROM " + tableTransaction + " WHERE order_id= " + order_id;
         ArrayList<Transaction> transactions = new ArrayList<>();
         try {
@@ -111,7 +219,7 @@ public class Order extends MyConnection{
             System.out.println(e.getMessage());
         }
         return transactions;
-    }
+    }*/
 
 
 
@@ -120,11 +228,29 @@ public class Order extends MyConnection{
     }
 
     public boolean update() {
-        return false;
+        String now_date= generateDate();
+        String query = "UPDATE "+ tableName + " SET status='"+ this.status + "', updated_at = '"+ now_date +"' WHERE id = " + this.id;
+        try {
+            Statement stmt = this.conn().createStatement();
+            return stmt.executeUpdate(query) > 0;
+        } catch (SQLException e) {
+            System.out.println("update order() : " + e.getMessage());
+            return false;
+        }
     }
     
     public boolean delete() {
-        return false;
+        String query = "DELETE FROM " + tableName + " WHERE id = " + this.id;
+        try {
+            Statement stmt = this.conn().createStatement();
+            stmt.executeUpdate(query);
+            query = "DELETE FROM "+ tableTransaction+ " WHERE id_order = " + this.no;
+            Statement st = this.conn().createStatement();
+            return st.executeUpdate(query) > 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
     
     public Order findByOrderNo(int no){
@@ -136,10 +262,10 @@ public class Order extends MyConnection{
             if (res.next()) {
                 order.setId(res.getInt("id"));
                 order.setNo(res.getInt("no"));
-                order.setUser(res.getInt("user_id"));
+                order.setUser_id(res.getInt("user_id"));
                 order.setStatus(res.getString("status"));
-                order.setCreatedAt(res.getDate("created_at"));
-                order.setUpdatedAT(res.getDate("updated_at"));
+                order.setCreated_at(res.getDate("created_at"));
+                order.setUpdated_at(res.getDate("updated_at"));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -155,8 +281,8 @@ public class Order extends MyConnection{
             if (res.next()) {
                 order.setId(res.getInt("id"));
                 order.setNo(res.getInt("no"));
-                order.setCreatedAt(res.getDate("created_at"));
-                order.setUpdatedAT(res.getDate("updated_at"));
+                order.setCreated_at(res.getDate("created_at"));
+                order.setUpdated_at(res.getDate("updated_at"));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -173,13 +299,12 @@ public class Order extends MyConnection{
             if (res.next()) {
                 order.setId(res.getInt("id"));
                 order.setNo(res.getInt("no"));
-                order.setUser(res.getInt("user_id"));
+                order.setUser_id(res.getInt("user_id"));
                 order.setStatus(res.getString("status"));
-                order.setCreatedAt(res.getDate("created_at"));
-                order.setUpdatedAT(res.getDate("updated_at"));
-            }
-            else{
-                order.setUser(user_id);
+                order.setCreated_at(res.getDate("created_at"));
+                order.setUpdated_at(res.getDate("updated_at"));
+            }else{
+                order.setUser_id(user_id);
                 if(order.create()){
                    order= order.findByOrderNo(order.getNo());
                 }
@@ -190,17 +315,23 @@ public class Order extends MyConnection{
         return order;
     }
    
-    private boolean create() {
+    public boolean create() {
         String now_date= generateDate();
         String status= "new";
-        String query = "INSERT INTO "+ tableName +"(no, user_id, created_at, updated_at,status) values ('" + this.generateNo() + "', '" + this.user_id  + "', '" +  now_date + "', '" + now_date + "','" + status + "')";
-        Order order = new Order();
+        this.setNo(this.generateNo());
+        String query = "INSERT INTO "+ tableName +
+                "(no, user_id, buyer_id, created_at, updated_at,status) values ('" 
+                + this.getNo() + "', '"+ this.user_id+"', '" + this.buyer_id  + "', '" +  now_date + "', '" + now_date + "','" + status + "')";
+        //Order order = new Order();
         try {
             Statement stmt = this.conn().createStatement();
             return stmt.executeUpdate(query) > 0;
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println("create Order() : "+e.getMessage());
         }
         return false;
     }
+
+    
+    
 }
--- kamumau/src/java/model/Transaction.java
+++ kamumau/src/java/model/Transaction.java
@@ -1,22 +1,216 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author x201
- * CREATE TABLE orders (
-    id int NOT NULL AUTO_INCREMENT,
-    no varchar(10) NOT NULL,
-    user_id int NOT NULL.
-    created_at DATE NOT NULL,
-    updated_at DATE NOT NULL,
-    PRIMARY KEY (id)
-);
- */
-public class Transaction {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/*
+TABLE NAME transaction
+
+column
+id INT AUTO INCREMENTS
+id_order Long
+id_product INT
+qty INT
+total INT
+*/
+
+public class Transaction extends MyConnection{
+    private int id_order;
+    private int id_product;
+    private int qty;
+    private int total;
+    private String name;
+    private static final String TABLE_NAME = "transaction";
+    private static final String TABLE_NAME_ORDER = "orders";
+
+    public Transaction() {
+    }
+
+    public Transaction(int id_order, int id_product, int qty, int total) {
+        this.id_order = id_order;
+        this.id_product = id_product;
+        this.qty = qty;
+        this.total = total;
+    }
+
+    public int getId_order() {
+        return id_order;
+    }
+
+    public void setId_order(int id_order) {
+        this.id_order = id_order;
+    }
+
+    public int getId_product() {
+        return id_product;
+    }
+
+    public void setId_product(int id_product) {
+        this.id_product = id_product;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    public ArrayList<Transaction> all(int order_id){
+        //String query = "SELECT * FROM " + TABLE_NAME  + " WHERE id_order = " + order_id;
+//        String query2 = "SELECT o.id,o.no,o.user_id,o.buyer_id,u.full_name as seller_name, o.created_at, o.updated_at, o.status FROM orders o \n" +
+//        "INNER JOIN users u ON o.user_id = u.id WHERE o.buyer_id = "+user_id+";";
+//        
+        
+        String query = "SELECT t.id, t.id_order, t.id_product, p.name, t.qty, t.total "
+                + "FROM transaction t INNER JOIN products p ON t.id_product = p.id WHERE id_order = "+ order_id;
+        
+        
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        try {
+            Statement stmt = this.conn().createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            while (res.next()) {
+                Transaction tr = new Transaction();
+                tr.setId(res.getInt("id"));
+                tr.setId_product(res.getInt("id_product"));
+                tr.setId_order(res.getInt("id_order"));
+                tr.setId(res.getInt("id"));
+                tr.setQty(res.getInt("qty"));
+                tr.setTotal(res.getInt("total"));
+                tr.setName(res.getString("name"));
+                transactions.add(tr);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return transactions;
+    }
+    
+    public Transaction find(int id){
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE id = " + id;
+        Transaction tr = new Transaction();
+        try {
+            Statement stmt = this.conn().createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            if (res.next()) {
+                tr.setId(res.getInt("id"));
+                tr.setId_product(res.getInt("id_product"));
+                tr.setId_order(res.getInt("id_order"));
+                tr.setId(res.getInt("id"));
+                tr.setQty(res.getInt("qty"));
+                tr.setTotal(res.getInt("total"));
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return tr;
+    }
+    
+    
+    public boolean initOrCeate(Transaction trans, int user_id,int seller_id){
+        //Kelompok product bisa lihat method ini
+        System.out.println("Seller id "+seller_id);
+        String query = "SELECT * FROM " + TABLE_NAME_ORDER + " WHERE user_id = " + seller_id + 
+                " AND buyer_id = "+user_id+" AND status='new'";
+        try {
+            Statement stmt = this.conn().createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            Order order = new Order();
+            if (res.next()) {
+                order.setId(res.getInt("id"));
+                order.setNo(res.getInt("no"));
+                order.setUser_id(res.getInt("user_id"));
+                order.setBuyer_id(res.getInt("buyer_id"));
+                order.setStatus(res.getString("status"));
+                order.setCreated_at(res.getDate("created_at"));
+                order.setUpdated_at(res.getDate("updated_at"));
+                order.update();
+                trans.setId_order(res.getInt("no"));
+                trans.create();
+            }else{
+                order.setUser_id(seller_id);
+                order.setBuyer_id(user_id);
+                if(order.create()){
+                   int key = order.getNo();
+                   System.out.println("Generated : "+order.getNo());
+                   trans.setId_order(order.getNo());
+                   trans.create();
+                }
+            }
+            
+            return true;
+        } catch (SQLException e) {
+            System.out.println("initOrCreateTrans() : "+e.getMessage());
+            return false;
+        }
+    }
+    
+    
+    public boolean create() {
+        String query = "INSERT INTO "+ TABLE_NAME +" (id_order, id_product, qty, total) "
+                + "values ('" + this.id_order + "', '" + this.id_product  + "', '" +  this.qty  + "', '"+ this.total + "')";
+        try {
+            Statement stmt = this.conn().createStatement();
+            return stmt.executeUpdate(query) > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean update() {
+        String query = "UPDATE "+ TABLE_NAME + " SET id_product='"+ this.id_product + 
+                "', qty = '"+ this.qty +"', total = '"+this.total+"' WHERE id = " + this.id;
+        try {
+            Statement stmt = this.conn().createStatement();
+            return stmt.executeUpdate(query) > 0;
+        } catch (SQLException e) {
+            System.out.println("update() : "+ e.getMessage());
+            return false;
+        }
+    }
+    
+    public boolean delete() {
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE id = " + this.id + " ";
+        try {
+            Statement stmt = this.conn().createStatement();
+            return stmt.executeUpdate(query) > 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+    
     
 }
