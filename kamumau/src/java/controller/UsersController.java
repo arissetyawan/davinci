e5c0d457ea0679ee03c438b273f46f7b0c090b55
Dhiya Reksa Kusumojati Brameswanto <dhiyoreksa12@gmail.com>
2018-11-02 18:12:13 +0700

    Add files via upload
    User View

--- kamumau/src/java/controller/UsersController.java
+++ kamumau/src/java/controller/UsersController.java
@@ -7,18 +7,29 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.User;
 
 /**
  *
  * @author x201
  */
-@WebServlet(name = "UsersController", urlPatterns = {"/UsersController"})
-public class UsersController extends HttpServlet {
+@WebServlet(name = "UsersController", urlPatterns = {"/users"})
+public class UsersController extends ApplicationController {
+    private final static String add_action ="new";
+    private final static String login_action = "login";
+    private final static String logout_action = "logout";
+    private final static String list_action = "list";
+    private final static String Update_action ="update";
+    private String message="";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,20 +43,80 @@ public class UsersController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UsersController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UsersController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String action = request.getParameter("action");
+        if(action==null){
+            action= "login";
+        }
+        try{
+        switch (action) {
+            case "new":
+                ShowNewForm(request, response);
+                break;
+            case "create":
+                createUser(request, response);
+                break;
+            case "logout":
+                doLogout(request, response);
+                break;
+            case "do-login":
+                doLogin(request, response);
+                break;
+            case "welcome":
+                mustLoggedIn(request, response);
+                showWelcomePage(request, response);
+                break;
+            case "profile":
+                mustLoggedIn(request, response);
+                showProfileForm(request, response);
+                break;
+                
+            case "update":
+                mustLoggedIn(request, response);
+                updateProfile(request, response);
+            default:
+                showLoginForm(request, response);
+                break;
+            }
+        }
+        catch (SQLException ex){
+            throw new ServletException(ex);
         }
     }
 
+    private void doLogin(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException, ServletException  {
+                String email= request.getParameter("email");
+                String passwd= request.getParameter("password");
+                User user= new User();     
+                user.setEmail(email);
+                System.out.println("Isi email : " + email + " passwordnya : "+ passwd);
+                
+                user.setPassword(passwd);
+                logout(request, response);
+                if(user.doLogin()){
+                    setLoggedIn(user, request, response);
+                    showWelcomePage(request, response);
+                    System.out.println("Login berhasil.");
+                    
+                }else{
+                    showLoginFail(request, response);
+                    System.out.println("Login gagal.");
+                }
+        }
+    private void doLogout(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException, ServletException  {
+                logout(request, response);
+                showLoginForm(request, response);
+        }
+    private void showLoginForm(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException, ServletException  {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/users/login.jsp");
+            dispatcher.forward(request, response);
+        }
+    private void showWelcomePage(HttpServletRequest request, HttpServletResponse response)    throws ServletException, IOException, ServletException  {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("users/welcome.jsp");
+            dispatcher.forward(request, response);
+        }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -85,4 +156,92 @@ public class UsersController extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    private void ShowNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException,IOException{
+        RequestDispatcher dispatcher = request.getRequestDispatcher("users/signup.jsp");
+        dispatcher.forward(request, response);
+    }
+    
+    private void showProfileForm(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+        HttpSession session = request.getSession();
+        String me = session.getAttribute("current_user").toString();
+        User user = new User();
+        user = user.find(Integer.parseInt(me));
+        request.setAttribute("user", user);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("users/profile.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void createUser(HttpServletRequest request, HttpServletResponse response) throws SQLException,IOException,ServletException{
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String retpassword = request.getParameter("retpassword");
+        String fullname = request.getParameter("fullname");
+        String address = request.getParameter("address");
+        String bankname = request.getParameter("bankname");
+        String accountno = request.getParameter("accountno");
+        String created_at = request.getParameter("created_at");
+        
+        User user= new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRetpassword(retpassword);
+        user.setFullname(fullname);
+        user.setAddress(address);
+        user.setBankname(bankname);
+        user.setAccountno(accountno);
+        
+        if(user.create()){
+            message="new user added";
+            request.setAttribute("message", message);
+            response.sendRedirect("users?action="+add_action);
+        }else{
+            message = "new user failed to add";
+            request.setAttribute("message", message);
+            request.getRequestDispatcher("users?action="+add_action).include(request, response);
+        }
+    }
+
+    private void showLoginFail(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException, ServletException  {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/users/welcome.jsp");
+            dispatcher.forward(request, response);
+    }
+    
+        private void updateProfile(HttpServletRequest request, HttpServletResponse response) throws SQLException,IOException,ServletException{
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String retpassword = request.getParameter("retpassword");
+        String fullname = request.getParameter("fullname");
+        String address = request.getParameter("address");
+        String bankname = request.getParameter("bankname");
+        String accountno = request.getParameter("accountno");
+        String created_at = request.getParameter("created_at");
+        
+        HttpSession session = request.getSession();
+        String me = session.getAttribute("current_user").toString();
+        
+        User user= new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRetpassword(retpassword);
+        user.setFullname(fullname);
+        user.setAddress(address);
+        user.setBankname(bankname);
+        user.setAccountno(accountno);
+        user.setId(Integer.parseInt(me));
+        if(user.update()){
+            message="update";
+            request.setAttribute("message", message);
+            response.sendRedirect("users?action="+Update_action);
+        }else{
+            message = " user failed to update";
+            request.setAttribute("message", message);
+            request.getRequestDispatcher("users?action="+Update_action).include(request, response);
+        }
+    }
+
+    
 }
+    
+
